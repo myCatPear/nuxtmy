2aacfd517711792bb8194f8a142b0941b2fdcb80
Denis Tavabilov <dagger1233@gmail.com>
2025-07-30 23:39:18 +0500

    first commit

---
+++ app/pages/posts/[id].vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const params =  useRoute().params
+const id = params.id as string
+</script>
+
+<template>
+  <div>
+    Id: {{ id }}
+  </div>
+</template>
+
---
+++ app/pages/posts/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    All Posts    
+  </div>
+</template>
+
